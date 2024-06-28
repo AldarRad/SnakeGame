368b0aadb79bfbc3c92ce3a02216df00eced560c
Ilham <rald2005@mail.ru>
2024-06-28 13:48:13 +0800

    Дизайн Игры

---
+++ SNAKEGAME/Game.cpp
@@ -0,0 +1,12 @@
+#include "Game.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	SNAKEGAME::Game form;
+	Application::Run(% form);
+}
---
+++ SNAKEGAME/Game.h
@@ -0,0 +1,94 @@
+#pragma once
+#include <time.h>
+#include <stdlib.h> 
+namespace SNAKEGAME {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ Game
+	/// </summary>
+	public ref class Game : public System::Windows::Forms::Form
+	{
+	public:
+		Game(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~Game()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+		virtual void OnPaint(System::Object^ lbj, PaintEventArgs^ e) override
+		{
+			Graphics^ g = e->Graphics;
+
+			Brush^ fruitBrush = gcnew SolidBrush(Color::Red);
+			g->FillRectangle(fruitBrush, fruitPosition.X, fruitPosition.Y, blockSize, blockSize);
+
+			Brush^ snakeBrush = gcnew SolidBrush(Color::Green);
+			for each (Point el in snake)
+				g->FillRectangle(snakeBrush, el.X, el.Y, blockSize, blockSize);
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+		List<Point>^ snake;
+		Point fruitPosition;
+		const int blockSize = 20;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(400, 400);
+			this->Text = L"Game"; 
+			this->BackColor = Color::Black; 
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+
+			snake = gcnew List<Point>();
+			snake->Add(Point(200, 200));
+
+			srand(time(NULL));
+			PlaceFruit();
+
+			this->Paint += gcnew PaintEventHandler(this, &GameForm::OnPaint);
+		}
+#pragma endregion
+		void PlaceFruit()
+		{
+			int maxX = this->ClientSize.Width / blockSize;
+			int maxY = this->ClientSize.Height / blockSize;
+
+			do
+			{
+				fruitPosition = Point(rand() % maxX * blockSize, rand() % maxY * blockSize);
+			} while (snake->Contains(fruitPosition));
+
+		}
+	};
+}
